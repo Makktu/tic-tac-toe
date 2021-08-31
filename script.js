b43ad69f31ab1519c644b060e28079297eb205eb
Makktu <johnmcna@gmail.com>
2021-08-31 19:10:55 +0100

    Set up the basic board and interface

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const Gameboard = {
+    sq1: "X",
+    sq2: "O",
+    sq3: "O",
+    sq4: "X",
+    sq5: "O",
+    sq6: "X",
+    sq7: "O",
+    sq8: "O",
+    sq9: "O",
+};
+
+console.log(Gameboard);
+
+// * set up starting values
