84903b6dd7266795269dcaf80eda3c712ad6ccc0
Makktu <johnmcna@gmail.com>
2021-09-01 18:53:35 +0100

    almost finished the basic setup

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 "use strict";
 
+function updateBoard(squareClickedOn) {
+    console.log(squareClickedOn);
+    gameSquares.forEach(square, (ind) => {
+        square.textContent = Gameboard[ind];
+    });
+}
+
+function clickLocation(clickLoc) {
+    let theSquareClicked;
+    let xRow;
+    let yRow;
+    const xCoordinate = clickLoc.offsetX;
+    const yCoordinate = clickLoc.offsetY;
+    console.log(xCoordinate, yCoordinate);
+    if (xCoordinate < 130) {
+        xRow = 1;
+    } else if (xCoordinate > 155 && xCoordinate < 278) {
+        xRow = 2;
+    } else if (xCoordinate > 303) {
+        xRow = 3;
+    } else return;
+
+    if (yCoordinate < 134) {
+        yRow = 1;
+    } else if (yCoordinate > 153 && yCoordinate < 276) {
+        yRow = 2;
+    } else if (yCoordinate > 299) {
+        yRow = 3;
+    } else return;
+
+    // theSquareClicked = xCoordinate;
+    console.log(yRow, xRow);
+}
+
+const theGrid = document.querySelector(".the__grid");
+
+theGrid.addEventListener("click", (e) => {
+    clickLocation(e);
+});
+
 const Gameboard = {
     sq1: "X",
     sq2: "O",
@@ -12,6 +52,15 @@ const Gameboard = {
     sq9: "O",
 };
 
-console.log(Gameboard);
+const gameSquares = document.querySelectorAll(".sq");
+
+console.log(gameSquares);
+
+gameSquares.forEach((square) => {
+    console.log(square);
+    square.addEventListener("click", (e) => {
+        updateBoard(e.target.className);
+    });
+});
 
 // * set up starting values
